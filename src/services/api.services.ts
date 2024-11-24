f414400e705b7f9cb9fc7d42e46ce41d1634359d
SaitiUchiha <vira.brods@gmail.com>
2024-11-24 22:20:11 +0200

    Finished project

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -6,7 +6,7 @@ export const options = {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGM1ZjVmOGMwZTkzMWE5ZTYyNTdmMDUxMTk2NDI3NyIsIm5iZiI6MTczMTY5NTUyNi40NDQ4NjQsInN1YiI6IjY3MzYzODI3ZTA1ODIzYmQ5YzdjNTY3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V173sryojpOG9yX4qB4rs6P7skD5Wm1DqlvsFbz7KZM'
     },
-    next: {revalidate: 60}
+    next: {revalidate: 120}
 };
 
 export const fetchGenres: IGenreFetch = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
