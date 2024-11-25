8a4fa13f80f42d90c9143f2a1f38540c1132f960
SaitiUchiha <vira.brods@gmail.com>
2024-11-25 04:11:13 +0200

    Finished project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-// import {fetchPopularMovies} from "@/services/api.services";
-// import MoviesList from "@/components/movie/MoviesList";
-// import './globals.css'
-//
-// export default function Home() {
-//
-//     return (
-//         <div className="home-page">
-//             <h1>Popular</h1>
-//             <MoviesList movies={fetchPopularMovies.results}/>
-//         </div>
-//     );
-// }
-
-
 import MoviesList from "@/components/movie/MoviesList";
 import {fetchMovies} from "@/services/api.services";
 import {PaginationComponent} from "@/components/movie/MovieListPagination";
